4074222985a292fe31777b9b52d4b0865b23d4b9
bastelbude-01 <mod3llfr3ak1mg3ist@web.de>
2024-01-06 01:47:35 +0100

    in work

--- spot_rc/setup.py
+++ spot_rc/setup.py
@@ -14,8 +14,10 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*')),
-        (os.path.join('share', package_name, 'config'), glob('config/*'))
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'spot_rc'), glob('spot_rc/*'))
     ],
+    
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ros2ws',
